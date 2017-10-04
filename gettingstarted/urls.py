d5e478c04973d16f86ec54b88f804a9c2400d105
Neil Manvar <ndmanvar@yahoo.com>
2017-10-03 17:14:45 -0700

    updating

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -4,8 +4,6 @@ from rest_framework import routers
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
-
 from quickstart import views
 
 router = routers.DefaultRouter()
@@ -19,8 +17,4 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-
-    # url(r'^$', hello.views.index, name='index'),
-    url(r'^db', hello.views.db, name='db'),
-    url(r'^admin/', include(admin.site.urls)),
 ]
